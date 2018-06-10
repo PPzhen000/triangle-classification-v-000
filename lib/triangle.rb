714a384a156d8a0c5956e99c0883e2219d9cdc40
Ariel Zhen <zhenpingping@live.com>
2018-06-10 05:06:31 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,8 +11,6 @@ class Triangle
   def kind
     if (@a <= 0 || @b <= 0 || @c <= 0) || !(@a + @b > @c) || !(@a + @c > @b) || !(@b + @c > @a)
       raise TriangleError
-    # elsif !((@a + @b > @c) || (@a + @c > @b) || (@b + @c > @a))
-    #   raise TriangleError
     elsif  @a != @b && @a != @c && @b != @c
       :scalene
     elsif ((@a == @c || @b == @c) && @a != @b) ||
@@ -21,18 +19,6 @@ class Triangle
     elsif @a == @b && @a == @c && @b == @c
       :equilateral
     end
-    # if @a != @b && @a != @c && @b != @c
-    #   :scalene
-    # elsif ((@a == @c || @b == @c) && @a != @b) ||
-    #       ((@a == @b || @a == @c) && @b != @c)
-    #   :isosceles
-    # elsif @a == @b && @a == @c && @b == @c
-    #   :equilateral
-    # elsif @a == 0 || @b == 0 || @c == 0
-    #   raise TriangleError
-    # elsif !((@a + @b > @c) || (@a + @c > @b) || (@b + @c > @a))
-    #   raise TriangleError
-    # end
   end
 
   class TriangleError < StandardError
