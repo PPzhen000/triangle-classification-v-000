6b2040828aeededfa1803617a9154f5d95fb7897
Ariel Zhen <zhenpingping@live.com>
2018-06-10 04:48:04 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
   def kind
     if @a != @b && @a != @c && @b != @c
       :scalene
-    elsif @a == @b || @b == @c && @a != @b 
+    elsif @a == @b || @b == @c && @a != @b
       :isosceles
     elsif @a == @b && @a == @c && @b == @c
       :equilateral
