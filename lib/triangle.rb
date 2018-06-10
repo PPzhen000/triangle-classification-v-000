90b1ed99e2f98bffe1139c846f5201b80b256fe7
Ariel Zhen <zhenpingping@live.com>
2018-06-10 04:47:57 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,24 +1,24 @@
 class Triangle
 
-  attr_accessor :a, :b, :c 
+  attr_accessor :a, :b, :c
 
   def initialize(a, b, c)
-    @a = a 
-    @b = b 
-    @c = c 
-  end 
+    @a = a
+    @b = b
+    @c = c
+  end
 
-  def kind 
-    if @a != @b && @a != @c && @b != @c 
-      :scalene 
-    elsif @a == @b && @a != @c && @b != @c
+  def kind
+    if @a != @b && @a != @c && @b != @c
+      :scalene
+    elsif @a == @b || @b == @c && @a != @b 
       :isosceles
     elsif @a == @b && @a == @c && @b == @c
       :equilateral
-    end 
-  end 
+    end
+  end
 
   class TriangleError < StandardError
 
-  end 
+  end
 end
