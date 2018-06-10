7c833b21e1d75feeb3726e3bb10fff5bec06dd13
Ariel Zhen <zhenpingping@live.com>
2018-06-10 04:59:52 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,8 +16,8 @@ class Triangle
       :isosceles
     elsif @a == @b && @a == @c && @b == @c
       :equilateral
-    elsif !(@a > 0 && @b > 0 && @c > 0) 
-      raise TriangleError 
+    elsif !(@a > 0 && @b > 0 && @c > 0)
+      raise TriangleError
     elsif !((@a + @b > @c) || (@a + @c > @b) || (@b + @c > @a))
       raise TriangleError 
     end
